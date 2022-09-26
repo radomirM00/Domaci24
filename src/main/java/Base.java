43897893b9a74dc98a5c82deb0c022286fb214d0
radomirM00 <mlvcrdmr@gmail.com>
2022-09-26 04:56:09 +0200

    implemented new methods, TODO refactoring

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -37,6 +37,35 @@ public class Base {
         //driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"add_to_cart\"]/button"))).click();
     }
 
+    public static void proceedBtn() throws InterruptedException {
+        Thread.sleep(5000);
+        WebElement body = driver.findElement(By.tagName("body"));
+        WebElement row = body.findElement(By.className("button-container")).findElement(By.tagName("span"));
+        WebElement btn = row.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/span"));
+        btn.click();
+    }
+
+    public static void goToCart(){
+        WebElement cartLink = driver.findElement(By.xpath("//*[@id=\"header\"]/div[3]/div/div/div[3]/div/a"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(cartLink);
+        cartLink.click();
+    }
+
+    public static void removeFirstItemFromCart(){
+        WebElement firstElement = driver.findElement(By.id("product_1_1_0_0"));
+        WebElement removeBtn = firstElement.findElement(By.xpath("//*[@id=\"product_1_1_0_0\"]/td[7]/div/a"));
+        removeBtn.click();
+    }
+
+    public static void incrementQuantity(){
+        WebElement cartElement = driver.findElement(By.id("product_2_7_0_0"));
+        WebElement addBtn = cartElement.findElement(By.xpath("//*[@id=\"cart_quantity_up_2_7_0_0\"]"));
+        addBtn.click();
+    }
+
+
+
 
 
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,30 @@ public class Main extends Base{
 
     public static void main(String[] args) throws InterruptedException {
         Main test = new Main();
-        test.previewElement("//*[@id=\"homefeatured\"]/li[1]/div/div[1]/div/a[1]/img");
-        test.addToCart();
+        previewElement("//*[@id=\"homefeatured\"]/li[1]/div/div[1]/div/a[1]/img");
+        addToCart();
+        proceedBtn();
+
+        previewElement("//*[@id=\"homefeatured\"]/li[2]/div/div[1]/div/a[1]/img");
+        addToCart();
+        proceedBtn();
+
+        previewElement("//*[@id=\"homefeatured\"]/li[3]/div/div[1]/div/a[1]/img");
+        addToCart();
+        proceedBtn();
+
+        previewElement("//*[@id=\"homefeatured\"]/li[6]/div/div[1]/div/a[1]/img");
+        addToCart();
+        proceedBtn();
+
+        goToCart();
+        removeFirstItemFromCart();
+
+        incrementQuantity();
+
+
+
+
 
     }
 
